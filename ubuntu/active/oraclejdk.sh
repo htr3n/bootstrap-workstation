b32f18a97f99eec52828618d4c7162181f2efb40
Huy Tran <hoang.huy.tran@gmail.com>
2019-07-08 12:32:42 +1000

    revise the installation of oracle jdk

--- ubuntu/active/oraclejdk.sh
+++ ubuntu/active/oraclejdk.sh
@@ -10,8 +10,9 @@ sudo add-apt-repository -y -u ppa:linuxuprising/java
 # auto accept license
 echo oracle-java11-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
 
-sudo apt install -y oracle-java11-installer
-sudo apt install -y oracle-java11-set-default
+# https://www.linuxuprising.com/2019/06/new-oracle-java-11-installer-for-ubuntu.html
+#sudo apt install -y oracle-java11-installer
+#sudo apt install -y oracle-java11-set-default
 
-#sudo apt install -y oracle-java12-installer
-#sudo apt install oracle-java12-set-default
+sudo apt install -y oracle-java12-installer
+sudo apt install oracle-java12-set-default
